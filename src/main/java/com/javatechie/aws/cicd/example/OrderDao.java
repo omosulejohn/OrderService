ed62c082a47b170ebee9573406aff2a81a7bffc5
Simarjeet Singh Bhatia <96399536+bhatiasimarjeet@users.noreply.github.com>
2024-05-08 22:18:19 +0530

    Update OrderDao.java

--- src/main/java/com/javatechie/aws/cicd/example/OrderDao.java
+++ src/main/java/com/javatechie/aws/cicd/example/OrderDao.java
@@ -18,7 +18,8 @@ public class OrderDao {
                 new Order(809, "headset", 1, 1799),
                 new Order(111, "Car", 1, 200000),
                 new Order(112, "TV", 5, 400000),
-                new Order(113, "Headphones", 2, 500))
+                new Order(113, "Headphones", 2, 500),
+                new Order(114, "Refrigerator", 5, 2500))
                 .collect(Collectors.toList());
     }
 }
